2043375b36c7c25d9cfe973b3cf447a429d70d7a
Georgi Petrov <gap4400@gmail.com>
2023-09-19 21:57:18 +0300

    Refactor authentication of login users.

--- Forex Bet/TradingForecasts/src/main/java/forexbet/tradingforecasts/config/SecurityConfiguration.java
+++ Forex Bet/TradingForecasts/src/main/java/forexbet/tradingforecasts/config/SecurityConfiguration.java
@@ -31,12 +31,12 @@ public class SecurityConfiguration {
                                            SecurityContextRepository securityContextRepository) throws Exception {
         httpSecurity.authorizeHttpRequests()
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                .requestMatchers("/", "/about", "/contact", "/testimonial", "/forecast",
+                .requestMatchers("/", "/about", "/testimonial", "/forecast",
                         "/users/login-error", "/free-forecasts", "/api/free-forecasts", "/forecasts/ui",
-                        "/my-forecasts", "/active-forecasts", "/expired-forecasts",
-                        "/eur-usd", "/gold", "/dax", "/dow-jones", "/nasdaq").permitAll()
+                        "/expired-forecasts").permitAll()
                 .requestMatchers("/users/register", "/users/login", "/free-forecasts").anonymous()
-                .requestMatchers("/orders/order", "/my-account").authenticated()
+                .requestMatchers("/orders/order", "/my-account", "/my-forecasts", "/contact",
+                        "/active-forecasts", "/eur-usd", "/gold", "/dax", "/dow-jones", "/nasdaq").authenticated()
                 .requestMatchers("/orders/order", "/orders/order/expire/{id}").hasRole(UserRoleEnum.Moderator.name())
                 .requestMatchers("/forecasts/add", "/orders/order", "/orders/order/expire/{id}", "/change-role")
                 .hasRole(UserRoleEnum.Admin.name())
